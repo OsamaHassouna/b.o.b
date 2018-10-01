10d602423efcb959e4080af43f0ab148c9ee4acc
OsamaHassouna <eng.osama2021@gmail.com>
2018-10-01 16:58:23 +0200

    some fixes and Partners section

--- js/main.js
+++ js/main.js
@@ -25,5 +25,54 @@
 
 
 
+     /*[ Slick ]
+    ===========================================================*/
+    $('.slick').slick({
+        slidesToShow: 6,
+        slidesToScroll: 2,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                slidesToShow: 6,
+                slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                slidesToShow: 4,
+                slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 450,
+                settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+
 
 })(jQuery);
